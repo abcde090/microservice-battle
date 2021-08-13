4f327e61012148b3e1699fe775b3e1d14b8f9685
Dan Wang <dan.wang@kasna.com.au>
2021-08-13 15:09:36 +1000

    test our location

--- web.js
+++ web.js
@@ -3,11 +3,17 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+const URL = "https://nodejs-bot.34.67.217.211.nip.io";
 
 app.post("/", function (req, res) {
-  console.log("test our service", req.body._links.self.href);
+  console.log("test our service link", getOwnState(req));
   const moves = ["F", "T", "L", "R"];
   res.send("F");
 });
+
+let getOwnState = (req) => {
+  let collections = req.body.arena;
+  return collections;
+};
 
 app.listen(process.env.PORT || 8080);
