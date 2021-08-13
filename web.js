ea00c7a78fb1a57719739592111d40937975066e
Dan Wang <dan.wang@kasna.com.au>
2021-08-13 15:13:29 +1000

    test

--- web.js
+++ web.js
@@ -6,14 +6,14 @@ app.use(bodyParser.json());
 const URL = "https://nodejs-bot.34.67.217.211.nip.io";
 
 app.post("/", function (req, res) {
-  console.log("test our service link", getOwnState(req));
+  console.log("test our own service link", getOwnState(req));
   const moves = ["F", "T", "L", "R"];
   res.send("F");
 });
 
 let getOwnState = (req) => {
   let collections = req.body.arena;
-  return collections;
+  return collections[URL];
 };
 
 app.listen(process.env.PORT || 8080);
