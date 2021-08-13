14cf17bb41843322b0c888fb62a9cae7a851075f
Dan Wang <dan.wang@kasna.com.au>
2021-08-13 15:27:50 +1000

    test

--- web.js
+++ web.js
@@ -6,9 +6,9 @@ app.use(bodyParser.json());
 const URL = "https://nodejs-bot.34.67.217.211.nip.io";
 
 app.post("/", function (req, res) {
-  console.log("test our own service link", getOwnState(req));
+  const states = getOwnState(req);
   const moves = ["F", "T", "L", "R"];
-  res.send("F");
+  res.send("T");
 });
 
 let getOwnState = (req) => {
@@ -16,4 +16,28 @@ let getOwnState = (req) => {
   return collections[URL];
 };
 
+let moveDown = (meState) => {
+  switch (meState.direction) {
+    case "N":
+      return "R";
+    case "E":
+      return "R";
+    case "S":
+      return "F";
+    case "W":
+      return "L";
+    default:
+      return "F";
+  }
+};
+
+let getX = (state) => {
+  return state.x;
+};
+let getY = (state) => {
+  return state.y;
+};
+let getDirection = (state) => {
+  return state.direction;
+};
 app.listen(process.env.PORT || 8080);
